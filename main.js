67c709a3d75b1ee4f9f2ebd067649ea4e706c43a
Vito Leonelli <leonellivito444@gmail.com>
2026-04-18 16:01:23 -0300

    Next: Fix increment functionality

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-var currentStep = document.getElementById("step-display");
-if (currentStep) {
-    console.log("Found element: ".concat(typeof currentStep.textContent));
-}
-else {
-    console.error("Element with ID 'step-display' not found.");
-}
+// Counter and Step displays:
+var currentStepHTML = document.getElementById("step-display");
+var currentStepValue = Number(currentStepHTML.textContent);
+var currentCountHTML = document.getElementById("count-display");
+var currentCountValue = Number(currentCountHTML.textContent);
+// Interactives:
+var incrementButton = document.getElementById("increment-btn");
+var decrementButton = document.getElementById("decrement-btn");
+var resetButton = document.getElementById("reset-btn");
+var stepButton = document.getElementById("step-btn");
+var stepInput = document.getElementById("step-input");
+// Event Listeners:
+incrementButton.addEventListener('click', function () {
+    if (currentStepValue > 0) {
+        currentCountValue += currentStepValue;
+        currentCountHTML.textContent = String(currentCountValue);
+        console.log("Incremented value by ".concat(currentCountValue));
+    }
+    else {
+        // Create a error message that pops up if the step value is less than one!
+        var stepError = document.getElementById("step-error");
+        stepError.textContent = "The step value must be 1 or above!";
+        console.log("The step of ".concat(currentStepValue, " is invalid!"));
+    }
+    ;
+});
+// Methods:
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 // Counter and Step displays:
 const currentStepHTML: HTMLElement = document.getElementById("step-display") as HTMLElement;
-const currentStepValue: string = currentStepHTML.textContent;
+let currentStepValue: number = Number(currentStepHTML.textContent);
 
 const currentCountHTML: HTMLElement = document.getElementById("count-display") as HTMLElement;
-const currentCountValue: string = currentCountHTML.textContent;
+let currentCountValue: number = Number(currentCountHTML.textContent);
 
 // Interactives:
 const incrementButton: HTMLElement = document.getElementById("increment-btn") as HTMLElement;
@@ -14,6 +14,29 @@ const stepButton: HTMLElement = document.getElementById("step-btn") as HTMLEleme
 const stepInput: HTMLElement = document.getElementById("step-input") as HTMLElement;
 
 // Event Listeners:
+incrementButton.addEventListener('click', function() {
+    if (currentStepValue > 0) {
+        currentCountValue += currentStepValue;
+        currentCountHTML.textContent = String(currentCountValue);
+        console.log(`Incremented value by ${currentStepValue}`);
+    } else {
+        // Create a error message that pops up if the step value is less than one!
+        const stepError: HTMLElement = document.getElementById("step-error") as HTMLElement;
+        stepError.textContent = "The step value must be 1 or above!";
+        console.log(`The step of ${currentStepValue} is invalid!`);
+    };
+});
 
+decrementButton.addEventListener('click', function() {
+    // Decrements based on the step value...
+});
+
+resetButton.addEventListener('click', function() {
+    // Resets the count to 0 and step to 1...
+});
+
+stepButton.addEventListener('click', function() {
+    // Must validate on what the user has passes onto the step input box...
+});
 
 // Methods:
