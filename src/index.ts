de6c66f2a08e0fe18ef1da0a0b5e9a214e7b6c40
Vito Leonelli <leonellivito444@gmail.com>
2026-04-25 15:57:24 -0300

    Successfully initialized NPM & restructured folder structure

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+// Counter and Step displays:
+const currentStepHTML: HTMLElement = document.getElementById("step-display") as HTMLElement;
+const currentCountHTML: HTMLElement = document.getElementById("count-display") as HTMLElement;
+
+// Interactives:
+const incrementButton: HTMLElement = document.getElementById("increment-btn") as HTMLElement;
+const decrementButton: HTMLElement = document.getElementById("decrement-btn") as HTMLElement;
+const resetButton: HTMLElement = document.getElementById("reset-btn") as HTMLElement;
+const stepButton: HTMLElement = document.getElementById("step-btn") as HTMLElement;
+
+const stepInput: HTMLInputElement = document.getElementById("step-input") as HTMLInputElement;
+const stepError: HTMLElement = document.getElementById("step-error") as HTMLElement;
+
+// Constants:
+const DEFAULT_COUNT: number = 0;
+const DEFAULT_STEP: number = 1;
+
+// Tracking Variables:
+let count: number = DEFAULT_COUNT;
+let step: number = DEFAULT_STEP;
+let isErrorShown: boolean = false;
+
+// Event Listeners:
+incrementButton.addEventListener('click', function() {
+    count += step;
+    syncDisplay();
+    console.log(`Incremented value by ${step}`);
+});
+
+decrementButton.addEventListener('click', function() {
+    count -= step;
+    syncDisplay();
+    console.log(`Decremented value by ${step}`);
+});
+
+resetButton.addEventListener('click', function() {
+    count = DEFAULT_COUNT;
+    step = DEFAULT_STEP;
+    syncDisplay();
+});
+
+stepButton.addEventListener('click', function () {
+    const stepValue: number = stepInput.valueAsNumber;
+    
+    if (stepValue > 0) {
+        step = stepValue;
+        syncDisplay();
+        
+        if (isErrorShown) {
+            isErrorShown = false;
+            stepError.classList.remove("fade-in");
+            stepError.classList.add("fade-away");
+        }
+    } else if (!isErrorShown) {
+        isErrorShown = true;
+        stepError.classList.remove("fade-away");
+        stepError.classList.add("fade-in");
+    }
+});
+
+// Methods:
+function syncDisplay(): void {
+    // Sync the count and step values correctly into the UI!
+    currentCountHTML.textContent = String(count);
+    currentStepHTML.textContent = String(step);
+}
+
+// Function Calls:
+syncDisplay();
